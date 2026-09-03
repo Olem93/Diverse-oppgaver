fcc6dba73ac74eefcdc958ada0c63bd057605951
Olem93 <ole.overdal@gmail.com>
2026-09-03 14:39:58 +0200

    a
    a

---
+++ if-else-this/dynamisk knapp m ed tilstand/script.js
@@ -0,0 +1,24 @@
+//model
+let buttonThing = "";
+//view
+updateView();
+function updateView() {
+  document.getElementById("dynaknapp").innerHTML = /*HTML*/ `
+    <p>test</p>
+    <button onclick="summonButton()">Start test</button>
+    <button onclick="buttonColortest()">endre farge</button>
+
+    `;
+}
+
+// function buttonColorTest(){
+
+// }
+// function summonButton() {
+// if () {
+// get element by id inner html = / * html * /
+// <button></button>
+
+//  }
+// }
+//controller
