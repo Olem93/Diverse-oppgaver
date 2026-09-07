b7472820e56d32a39b5ba8610bec3b6c7788e1de
Olem93 <ole.overdal@gmail.com>
2026-09-07 12:56:37 +0200

    a
    a

--- if-else-this/dynamisk knapp m ed tilstand/script.js
+++ if-else-this/dynamisk knapp m ed tilstand/script.js
@@ -1,24 +1,60 @@
 //model
-let buttonThing = "";
+let buttonThing = 0;
+let isButtonSpawned = "";
+let dynaknapp = document.getElementById("dynaknapp");
+// let knappId = document.getElementById("knappId");
 //view
 updateView();
+
 function updateView() {
-  document.getElementById("dynaknapp").innerHTML = /*HTML*/ `
+  dynaknapp.innerHTML = /*HTML*/ `
     <p>test</p>
-    <button onclick="summonButton()">Start test</button>
-    <button onclick="buttonColortest()">endre farge</button>
+    
+    <div id="knappId">
+    <button onclick="summonButtom()">Spawner knapp</button>
+    </div>
+    <div id="spawnPoint"></div>
+
+    
 
     `;
 }
 
-// function buttonColorTest(){
+// controller
 
-// }
-// function summonButton() {
-// if () {
-// get element by id inner html = / * html * /
-// <button></button>
+function summonButtom() {
+  spawnPoint.innerHTML = /*HTML*/ `
+    <button id="spawnedButton" onclick="buttonColorchange(this)">test1</button>
+`;
+  copyKnapp(this);
+}
 
-//  }
-// }
-//controller
+function buttonColorchange(shift) {
+  if (buttonThing === 0) {
+    shift.style.background = "red";
+    shift.innerHTML = "Green";
+    buttonThing = 1;
+
+    // copyKnapp();
+  } else if (buttonThing === 1) {
+    shift.style.background = "green";
+    shift.innerHTML = "Blue";
+    buttonThing = 2;
+
+    // copyKnapp();
+  } else {
+    shift.style.background = "blue";
+    shift.innerHTML = "Red";
+    buttonThing = 0;
+
+    // copyKnapp();
+  }
+}
+
+function copyKnapp() {
+  let cloneVar = document.getElementById("knappId").innerHTML;
+  dynaknapp.innerHTML += cloneVar;
+}
+// let cloneVar = dynaknapp.innerHTML;
+// dynaknapp.innerHTML += cloneVar;
+// lage en komponent som genererer en knapp, hvor du da endrer innholdet til knappen med parameter
