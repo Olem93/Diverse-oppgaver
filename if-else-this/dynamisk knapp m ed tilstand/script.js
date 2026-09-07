c38c889d049b0e5d73e14acefba5d069826bf0ff
Olem93 <ole.overdal@gmail.com>
2026-09-07 14:36:13 +0200

    a
    a

--- if-else-this/dynamisk knapp m ed tilstand/script.js
+++ if-else-this/dynamisk knapp m ed tilstand/script.js
@@ -1,7 +1,7 @@
 //model
 let buttonThing = 0;
 let isButtonSpawned = "";
-let dynaknapp = document.getElementById("dynaknapp");
+let timeOut;
 // let knappId = document.getElementById("knappId");
 //view
 updateView();
@@ -11,9 +11,10 @@ function updateView() {
     <p>test</p>
     
     <div id="knappId">
-    <button onclick="summonButtom()">Spawner knapp</button>
+    <button onclick="summonButton()">Spawner knapp</button>
     </div>
     <div id="spawnPoint"></div>
+    
 
     
 
@@ -22,38 +23,38 @@ function updateView() {
 
 // controller
 
-function summonButtom() {
+function summonButton() {
   spawnPoint.innerHTML = /*HTML*/ `
     <button id="spawnedButton" onclick="buttonColorchange(this)">test1</button>
 `;
-  copyKnapp(this);
+  console.log(summonButton());
 }
 
-function buttonColorchange(shift) {
-  if (buttonThing === 0) {
-    shift.style.background = "red";
-    shift.innerHTML = "Green";
-    buttonThing = 1;
+// function buttonColorchange(shift) {
+//   if (buttonThing === 0) {
+//     shift.style.background = "red";
+//     shift.innerHTML = "Green";
+//     buttonThing = 1;
 
-    // copyKnapp();
-  } else if (buttonThing === 1) {
-    shift.style.background = "green";
-    shift.innerHTML = "Blue";
-    buttonThing = 2;
+//     // copyKnapp();
+//   } else if (buttonThing === 1) {
+//     shift.style.background = "green";
+//     shift.innerHTML = "Blue";
+//     buttonThing = 2;
 
-    // copyKnapp();
-  } else {
-    shift.style.background = "blue";
-    shift.innerHTML = "Red";
-    buttonThing = 0;
+//     // copyKnapp();
+//   } else {
+//     shift.style.background = "blue";
+//     shift.innerHTML = "Red";
+//     buttonThing = 0;
 
-    // copyKnapp();
-  }
-}
+//     // copyKnapp();
+//   }
+// }
 
 function copyKnapp() {
-  let cloneVar = document.getElementById("knappId").innerHTML;
-  dynaknapp.innerHTML += cloneVar;
+  let cloneVar = document.getElementById("spawnedButton").innerHTML;
+  spawnPoint.innerHTML += cloneVar;
 }
 // let cloneVar = dynaknapp.innerHTML;
 // dynaknapp.innerHTML += cloneVar;
